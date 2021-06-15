f2011054db2496d22c8d261395af87f138f08995
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-16 01:31:49 +0200

    importo file in index

--- classes/user_premium.php
+++ classes/user_premium.php
@@ -1,6 +1,9 @@
 <?php
 
+
+require_once (__DIR__ . "./user.php");
+
 class UserPremium extends User {
 
-    
+    protected $extraDiscount;
 }
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once ("./classes/user.php");
+require_once ("./classes/product.php");
+require_once ("./classes/user_premium.php");
+require_once ("./classes/credit_card.php");
+
