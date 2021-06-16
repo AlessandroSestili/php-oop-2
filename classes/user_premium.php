0d47c3e8c3cc2551c8b7575ec9a5782b8ab13208
AlessandroSestili <sestilialessandro@gmail.com>
2021-06-16 10:16:54 +0200

    set construct/parent user_premium

--- classes/user_premium.php
+++ classes/user_premium.php
@@ -6,4 +6,25 @@ require_once (__DIR__ . "/user.php");
 class UserPremium extends User {
 
     protected $extraDiscount;
+
+
+    function __construct($id ,$name , $surname, $email , $extraDiscount)
+    {
+        parent::__construct($id ,$name , $surname, $email);
+        
+
+    }
+
+
+    //SET & GET extradiscount
+    protected function setExtraDiscount($value){
+      
+        $this->extraDiscount = $value;
+    }
+  
+    protected function getExtraDiscount() {
+
+        return $this->extraDiscount;
+    }
 }
+
